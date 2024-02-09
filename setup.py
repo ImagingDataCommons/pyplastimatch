8df34cfdd1d343a9d5903dcc5355973636a6845f
denbonte <dennis_bontempi@dfci.harvard.edu>
2024-02-09 12:37:07 +0100

    Bump version 0.4.3 -> 0.4.4, update authors and maintainers

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
   name="pyplastimatch",
-  version="0.4.3",
+  version="0.4.4",
   description="Basic Python wrapper for Plastimatch",
   url="https://github.com/AIM-Harvard/pyplastimatch",
   author="Dennis Bontempi",
