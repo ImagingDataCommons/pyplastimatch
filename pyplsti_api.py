467221b2940a79c688136fd74404c4b7bd99303b
hosseinjafar <hosseinj.2555@gmail.com>
2025-01-22 11:49:03 -0500

    updated the input model in the api to handle paths to the temp_data

--- pyplsti_api.py
+++ pyplsti_api.py
@@ -27,11 +27,17 @@ class Inputs_register(BaseModel):
 
     global_params: Dict[str, str]
     stage_params_list: List[Dict[str, str]]
+    def __init__(self, **data):
+        super().__init__(**data)
+        dir_temp_data = Path(__file__).parent.joinpath("temp_data")
+        self.global_params["fixed"] = dir_temp_data.joinpath(self.global_params.get("fixed"))
+        self.global_params["moving"] = dir_temp_data.joinpath(self.global_params.get("moving"))
+        self.global_params["image_out"] = dir_temp_data.joinpath(self.global_params.get("image_out"))
 
 app = FastAPI()
 
-@app.post("/register")
-def register(
+@app.post("/plastimatch_register")
+def register_api(
     all_inputs: Inputs_register
     ) -> None:
     r"""
@@ -39,3 +45,26 @@ def register(
         - To run image registration using pyplastimatch.
     """
     register(all_inputs.global_params, all_inputs.stage_params_list)
+
+
+def test_register_api():
+    pth_static = "static.nrrd"
+    pth_moving = "moving.nrrd"
+    pth_output = "registered.nrrd"
+
+    global_params = {
+        "fixed" : f"{pth_static}",
+        "moving" : f"{pth_moving}",
+        "image_out" : f"{pth_output}",
+    }
+    
+    stage_params_list = [
+        {
+            "xform": "bspline"
+        }
+    ]
+    inputs = Inputs_register(global_params=global_params, stage_params_list=stage_params_list)
+    register_api(inputs)
+
+if __name__ == "__main__":
+    test_register_api()
