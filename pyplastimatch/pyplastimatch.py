7761f5d9631b69bc22f8a70c4bcbda0d84e12de4
Alex Triay Bagur <alexandre.triay_bagur@kcl.ac.uk>
2025-03-05 10:25:18 +0000

    handle multiple metadata tags in convert function

--- pyplastimatch/pyplastimatch.py
+++ pyplastimatch/pyplastimatch.py
@@ -44,12 +44,16 @@ def convert(verbose = True, path_to_log_file = None, return_bash_command = False
   bash_command += ["plastimatch", "convert"]
   
   for key, val in kwargs.items():
-    bash_command += ["--%s"%(key), val]
+      if key == "metadata" and isinstance(val, list):
+          for meta in val:
+              bash_command += ["--metadata", str(meta)]
+      else:
+          bash_command += [f"--{key}", str(val)]
   
   if verbose:
     print("\nRunning 'plastimatch convert' with the specified arguments:")
-    for key, val in kwargs.items():
-      print("  --%s"%(key), val)
+    for arg in bash_command[2:]:
+        print(f"  {arg}")
   
   try:
     if path_to_log_file:
@@ -58,7 +62,6 @@ def convert(verbose = True, path_to_log_file = None, return_bash_command = False
                                           stdout = log_file, stderr = log_file,
                                           check = True)
     else:
-      # if no log file is specified, output to the default stdout and stderr
       bash_exit_status = subprocess.run(bash_command, capture_output = True, check = True)
       
     if verbose: print("... Done.")
